5a8630d3a168a8ced2c8162c86413d2668fd57f7
Sabyasachi Biswal <sabyasachi.biswal1@gmail.com>
2020-11-18 21:37:00 +0800

    home screen post from firebase

--- app/src/main/java/com/sabya/instagram/activities/HomeActivity.kt
+++ app/src/main/java/com/sabya/instagram/activities/HomeActivity.kt
@@ -1,12 +1,30 @@
 package com.sabya.instagram.activities
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.StyleSpan
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.sabya.instagram.R
 import com.sabya.instagram.utils.FirebaseHelper
+import com.sabya.instagram.utils.GlideApp
 import com.sabya.instagram.utils.ValueEventListenerAdapter
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.feed_item.view.*
 
 class HomeActivity : BaseActivity(0) {
 
@@ -21,9 +39,7 @@ class HomeActivity : BaseActivity(0) {
 
         mFirebase = FirebaseHelper(this)
 
-        sign_out_text.setOnClickListener {
-            mFirebase.auth.signOut()
-        }
+
         mFirebase.auth.addAuthStateListener {
             if (it.currentUser == null) {
                 startActivity(Intent(this, LoginActivity::class.java))
@@ -31,20 +47,83 @@ class HomeActivity : BaseActivity(0) {
             }
         }
 
-        mFirebase.database.child("feed-post").child(mFirebase.auth.currentUser!!.uid)
-            .addValueEventListener(ValueEventListenerAdapter {
-                val posts = it.children.map { it.getValue(FeedPost::class.java) }
-                Log.d(TAG, "feedPosts: ${posts.first()!!.timeStampDate()})")
-            })
 
     }
 
     override fun onStart() {
         super.onStart()
-        if (mFirebase.auth.currentUser == null) {
+        val currentUser = mFirebase.auth.currentUser
+        if (currentUser == null) {
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
+        } else {
+            mFirebase.database.child("feed-post").child(currentUser.uid)
+                .addValueEventListener(ValueEventListenerAdapter {
+                    val posts = it.children.map { it.getValue(FeedPost::class.java)!! }
+                    Log.d(TAG, "feedPosts: ${posts.first().timeStampDate()})")
+                    feed_recycler.adapter = FeedAdapter(posts)
+                    feed_recycler.layoutManager = LinearLayoutManager(this)
+                })
+
         }
+    }
+}
+
+class FeedAdapter(private val posts: List<FeedPost>) :
+    RecyclerView.Adapter<FeedAdapter.ViewHolder>() {
+
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.feed_item, parent, false)
+        return ViewHolder(view)
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val post = posts[position]
+        with(holder) {
+            view.user_photo_image.loadImage(post.photo)
+            view.post_image.loadImage(post.image)
+            view.username_text.text = post.userName
+            if (post.likesCount == 0) {
+                view.likes_text.visibility = View.GONE
+            } else {
+                view.likes_text.visibility = View.VISIBLE
+                view.likes_text.text = "${post.likesCount} likes"
+            }
+            view.caption_text.setCaptionText(post.userName, post.caption)
+        }
+    }
+
+    private fun TextView.setCaptionText(userName: String, caption: String) {
+        val userNameSpannable = SpannableString(userName)
+        userNameSpannable.setSpan(
+            StyleSpan(Typeface.BOLD),
+            0,
+            userNameSpannable.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        userNameSpannable.setSpan(object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                widget.context.showToast("Username is clicked")
+            }
+
+            override fun updateDrawState(ds: TextPaint) {}
+
+        }, 0, userNameSpannable.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        text = SpannableStringBuilder().append(userNameSpannable).append(" ")
+            .append(caption)
+        movementMethod = LinkMovementMethod.getInstance()
+    }
+
+    override fun getItemCount() = posts.size
+
+    private fun ImageView.loadImage(image: String?) {
+        GlideApp.with(this).load(image).centerCrop().into(this)
     }
 
 }
